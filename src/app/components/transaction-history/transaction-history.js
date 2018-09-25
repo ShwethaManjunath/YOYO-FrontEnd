228a328933fca8cca7e838717f4f1e7db15df5e7
Shwetha M <M1045570@mindtree.com>
2018-09-25 18:16:48 +0530

    added transaction component

---
+++ src/app/components/transaction-history/transaction-history.js
@@ -0,0 +1,12 @@
+// basic react component starting template
+import React, { Component } from 'react';
+
+class TransactionHistory extends Component {
+    render() {
+        return (
+            <div>TransactionHistory Component</div>
+        );
+    }
+}
+
+export default TransactionHistory;
